f36cefafa0a7b2b53136dbb4bf127f81cd438ccb
ryanmadsenn <mad19002@byui.edu>
2022-04-27 10:27:23 -0600

    Made tic-tac-toe game

---
+++ tic_tac_toe/Program.cs
@@ -0,0 +1,129 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+const string x = "X";
+const string o = "O";
+const string blank = " ";
+
+string[] gameBoard = new string[] {blank, blank, blank, blank, blank, blank, blank, blank, blank};
+
+void playGame(){
+
+    while (gameDone(gameBoard) == false) {
+        // Display game board.
+        displayBoard(gameBoard);
+
+        // Figure out whose turn it is and assing that value to player.
+        bool turn = isXTurn(gameBoard);
+        string player = "";
+
+        if (isXTurn(gameBoard) == true) {
+            player = x;
+        } else {
+            player = o;
+        }
+
+        bool validInput = false;
+
+        // Prompt player for move.
+        while (validInput == false) {
+            try {
+                Console.Write($"{player}>");
+                string stringMove = Console.ReadLine();
+                int move = Convert.ToInt32(stringMove);
+
+                if (gameBoard[move-1] != " ") {
+                    Console.WriteLine("Space is already taken.");
+                } else {
+                    gameBoard[move-1] = player;
+                    validInput = true;
+                }
+            } 
+            catch {
+                Console.WriteLine("Not a valid space.");
+            }
+        }
+    }
+}
+
+
+bool gameDone(string[] board) {
+    // Check columns.
+    for (int i=0; i<3; i++) {
+        string space1 = board[0 + i];
+        string space2 = board[3 + i];
+        string space3 = board[6 + i];
+
+        if(space1 != blank && space2 != blank && space3 != blank && space1 == space2 && space2 == space3) {
+            displayBoard(board);
+            Console.WriteLine("The game was won by " + board[0 + i]);
+            return true;
+        }
+    }
+
+    // Check rows.
+    for (int i=0; i<7; i=i+3) {
+        string space1 = board[0 + i];
+        string space2 = board[1 + i];
+        string space3 = board[2 + i];
+
+        if(space1 != blank && space2 != blank && space3 != blank && space1 == space2 && space2 == space3) {
+            displayBoard(board);
+            Console.WriteLine("The game was won by " + board[0 + i]);
+            return true;
+        }
+    }
+
+    // Check diaganols.
+    for (int i=0; i<3; i=i+2) {
+        string space1 = board[0 + i];
+        string space2 = board[4];
+        string space3 = board[8 - i];
+
+        if(space1 != blank && space2 != blank && space3 != blank && space1 == space2 && space2 == space3) {
+            displayBoard(board);
+            Console.WriteLine("The game was won by " + board[0 + i]);
+            return true;
+        }
+    }
+
+    return false;
+
+}
+
+
+void displayBoard(string[] board) {
+    Console.WriteLine($"\n {board[0]} | {board[1]} | {board[2]} ");
+    Console.WriteLine("---+---+---");
+    Console.WriteLine($" {board[3]} | {board[4]} | {board[5]} ");
+    Console.WriteLine("---+---+---");
+    Console.WriteLine($" {board[6]} | {board[7]} | {board[8]}\n");
+}
+
+bool isXTurn(string[] board) {
+
+    int emptySpaces = 0;
+
+    foreach (string space in board) {
+        if (space == " ") {
+            emptySpaces++;
+        }
+    }
+
+    if (emptySpaces % 2 == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+Console.WriteLine("\nTIC-TAC-TOE\n");
+Console.WriteLine("You know the rules. Enter a number from 1 to 9 where numbers "); 
+Console.WriteLine("correspond to the following spaces. X goes first.\n");
+Console.WriteLine(" 1 | 2 | 3 ");
+Console.WriteLine("---+---+---");
+Console.WriteLine(" 4 | 5 | 6 ");
+Console.WriteLine("---+---+---");
+Console.WriteLine(" 7 | 8 | 9 \n");
+Console.WriteLine("The current board is:");
+playGame();
